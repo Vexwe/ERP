3a1d71b797d539c5045202e6e7c1f4ef96807c1f
jhonherikgc <jhonherik006@gmail.com>
2025-12-23 18:12:44 +0000

    :truck: center_path

---
+++ src/pages/Center/Center.jsx
@@ -0,0 +1,12 @@
+import Footer from "../../components/layout/Footer";
+import Header from "../../components/layout/Header";
+
+export default function Center(){
+    return(
+        <> 
+        <Header/>
+            <h1>olá</h1>
+        <Footer/>
+        </>
+    )
+}
