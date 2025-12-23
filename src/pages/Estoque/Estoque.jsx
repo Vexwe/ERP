bf470a2c00a1e209f478d2ef0137e84b279279f4
jhonherikgc <jhonherik006@gmail.com>
2025-12-23 18:12:34 +0000

    :truck: estoque_path

---
+++ src/pages/Estoque/Estoque.jsx
@@ -0,0 +1,12 @@
+import Footer from "../../components/layout/Footer";
+import Header from "../../components/layout/Header";
+
+export default function Estoque(){
+    return(
+        <> 
+        <Header/>
+            <h1>olá</h1>
+        <Footer/>
+        </>
+    )
+}
